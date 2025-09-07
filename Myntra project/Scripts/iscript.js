24082adac3514a0b0751d2d31cf1e86276d4b7f6
abhilashgit2003 <hemanthdavarasingi11@gmail.com>
2025-09-07 20:31:37 +0530

    Added functionality to the 'add to cart' button

--- Myntra project/Scripts/iscript.js
+++ Myntra project/Scripts/iscript.js
@@ -1,20 +1,30 @@
-let htmll='';
-items.forEach(item=>{
-  htmll+=`
-<div class="item-container">
-      <img class="item-image" src="${item.image}" alt="item image">
-      <div class="rating">
-          ${item.rating.stars} ⭐ | ${item.rating.count}
-      </div>
-      <div class="company-name">${item.company}</div>
-      <div class="item-name">${item.item_name}</div>
-      <div class="price">
-          <span class="current-price">Rs ${item.current_price}</span>
-          <span class="original-price">Rs ${item.original_price}</span>
-          <span class="discount">(${item.discount_percentage}% OFF)</span>
-      </div>
-      <button class="btn-add-bag" >Add to Bag</button>
-    </div>`});
+displayItemsOnHomePage();
 
-let itemsContainerElement = document.querySelector('.items-container');
-itemsContainerElement.innerHTML=htmll;
+let bagItems=[];
+function addToBag(itemId){
+  bagItems.push(itemId);
+}
+
+function displayItemsOnHomePage(){
+    let innerHtml='';
+    items.forEach(item=>{
+      innerHtml+=`
+    <div class="item-container">
+          <img class="item-image" src="${item.image}" alt="item image">
+          <div class="rating">
+              ${item.rating.stars} ⭐ | ${item.rating.count}
+          </div>
+          <div class="company-name">${item.company}</div>
+          <div class="item-name">${item.item_name}</div>
+          <div class="price">
+              <span class="current-price">Rs ${item.current_price}</span>
+              <span class="original-price">Rs ${item.original_price}</span>
+              <span class="discount">(${item.discount_percentage}% OFF)</span>
+          </div>
+          <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+        </div>`});
+
+    let itemsContainerElement = document.querySelector('.items-container');
+    itemsContainerElement.innerHTML=innerHtml;
+}
+
