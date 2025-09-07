e62f22252c90a19e2f180532ee90ea732f0734c9
abhilashgit2003 <hemanthdavarasingi11@gmail.com>
2025-09-07 19:38:21 +0530

    Added each item from items list in JS

--- Myntra project/Scripts/iscript.js
+++ Myntra project/Scripts/iscript.js
@@ -1,32 +1,20 @@
-const items = 
-      {
-        id: '001',
-        image: 'images/1.jpg',
-        company: 'Carlton London',
-        item_name: 'Rhodium-Plated CZ Floral Studs',
-        original_price: 1045,
-        current_price: 606,
-        discount_percentage: 42,
-        return_period: 14,
-        delivery_date: '10 Oct 2023',
-        rating: {
-            stars: 4.5,
-            count: 1400,
-        }
-      }
-let itemsContainerElement = document.querySelector('.items-container');
-itemsContainerElement.innerHTML=`
+let htmll='';
+items.forEach(item=>{
+  htmll+=`
 <div class="item-container">
-      <img class="item-image" src="${items.image}" alt="item image">
+      <img class="item-image" src="${item.image}" alt="item image">
       <div class="rating">
-          ${items.rating.stars} ⭐ | ${items.rating.count}
+          ${item.rating.stars} ⭐ | ${item.rating.count}
       </div>
-      <div class="company-name">${items.company}</div>
-      <div class="item-name">${items.item_name}</div>
+      <div class="company-name">${item.company}</div>
+      <div class="item-name">${item.item_name}</div>
       <div class="price">
-          <span class="current-price">Rs ${items.current_price}</span>
-          <span class="original-price">Rs ${items.original_price}</span>
-          <span class="discount">(${items.discount_percentage}% OFF)</span>
+          <span class="current-price">Rs ${item.current_price}</span>
+          <span class="original-price">Rs ${item.original_price}</span>
+          <span class="discount">(${item.discount_percentage}% OFF)</span>
       </div>
       <button class="btn-add-bag" >Add to Bag</button>
-    </div>`;
+    </div>`});
+
+let itemsContainerElement = document.querySelector('.items-container');
+itemsContainerElement.innerHTML=htmll;
