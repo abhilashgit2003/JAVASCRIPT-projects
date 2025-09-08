e08d1bf211bccd616c6ac95a9e4fdf61760389ea
abhilashgit2003 <hemanthdavarasingi11@gmail.com>
2025-09-08 11:40:58 +0530

    Added inner html to store the products on page

--- Myntra project/Scripts/bag.js
+++ Myntra project/Scripts/bag.js
@@ -6,7 +6,7 @@ function  onLoad(){
   displayBagItems();
 }
 
-function loadBagItemObjects(item){
+function loadBagItemObjects(){
 
   bagItemObjects=bagItems.map(itemId=>{
       for (let i=0;i<items.length;i++)
@@ -24,24 +24,33 @@ function loadBagItemObjects(item){
 
 function displayBagItems(){
   let containerElement=document.querySelector('.bag-items-container');
-  containerElement.innerHTML=`<div class="bag-item-container">
+  let innerHTML='';
+  bagItemObjects.forEach(bagItem => {
+    innerHTML+=generateItemHTML(bagItem);
+    
+  });
+   containerElement.innerHTML=innerHTML;
+}
+
+function generateItemHTML(item){
+  return `<div class="bag-item-container">
             <div class="item-left-part">
-              <img class="bag-item-img" src="../images/4.jpg">
+              <img class="bag-item-img" src="../${item.image}">
             </div>
             <div class="item-right-part">
-              <div class="company">ADIDAS</div>
-              <div class="item-name">Men Printed Polo Collar Indian Cricket ODI Jersey</div>
+              <div class="company">${item.company}</div>
+              <div class="item-name">${item.item_name}</div>
               <div class="price-container">
-                <span class="current-price">Rs 999</span>
-                <span class="original-price">Rs 999</span>
-                <span class="discount-percentage">(0% OFF)</span>
+                <span class="current-price">Rs ${item.original_price}</span>
+                <span class="original-price">Rs ${item.current_price}</span>
+                <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
               </div>
               <div class="return-period">
-                <span class="return-period-days">14 days</span> return available
+                <span class="return-period-days">${item.return_period} days</span> return available
               </div>
               <div class="delivery-details">
                 Delivery by
-                <span class="delivery-details-days">10 Oct 2023</span>
+                <span class="delivery-details-days">${item.delivery_date}</span>
               </div>
             </div>
 
